80014950566de13406ef704f5708a90490a0a57e
Maddy Rio <madison-rio@hotmail.com>
2024-01-26 18:24:21 +1300

    Add nav bar CSS

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,7 +1,11 @@
 function Nav() {
   return (
-    <header>
-      <h1>TrendyWoofz</h1>
+    <header className="nav">
+      <img className="logo" src="IMG.jpeg" />
+      <a href="/">Home</a>
+      <a href="/about">About Us</a>
+      <a href="/shop">Shop All</a>
+      <a href="/contact">Contact</a>
     </header>
   )
 }
