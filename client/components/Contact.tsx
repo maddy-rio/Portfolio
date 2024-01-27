b4fa694ecf76c68c2e2a52e956813212fded8691
Maddy Rio <madison-rio@hotmail.com>
2024-01-27 13:09:50 +1300

    Add contact form css

--- client/components/Contact.tsx
+++ client/components/Contact.tsx
@@ -5,8 +5,45 @@ function Contact() {
   return (
     <>
       <Nav />
-      <h1>Contact Us</h1>
-      <main></main>
+      <h1>Have More Questions?</h1>
+      <p className="form">
+        Get in touch with the TrendyWoofz team below! <br></br>
+        We'd love to hear from you and your pup 🐾
+      </p>
+      <div className="container">
+        <img src="Mail-icon.png" alt="Mail icon"></img>
+        <form>
+          <div className="body">
+            <ul>
+              <li>
+                <label>
+                  <input type="text" name="form" placeholder="First name" />
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input type="text" name="form" placeholder="Last Name" />
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input
+                    type="text"
+                    name="form"
+                    placeholder="What's your email?"
+                  />
+                </label>
+              </li>
+              <li>
+                <textarea rows={4} name="form" placeholder="How can we help?" />
+              </li>
+              <li>
+                <button type="submit">Send Message</button>
+              </li>
+            </ul>
+          </div>
+        </form>
+      </div>
       <Footer />
     </>
   )
