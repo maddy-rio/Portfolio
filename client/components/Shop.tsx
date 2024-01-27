7a8818ec96494f8b14bcaf3a98acbc4f6b91bdcb
Maddy Rio <madison-rio@hotmail.com>
2024-01-27 17:02:05 +1300

    Update css for shop page

--- client/components/Shop.tsx
+++ client/components/Shop.tsx
@@ -10,31 +10,31 @@ function Shop() {
         <h3>QuikHarness</h3>
         <p className="price">$40 NZD</p>
         <img src="Harness.png" alt="QuikHarness" />
-        <button>Add to Cart</button>
+   
       </div>
       <div className="card">
         <h3>Woofz Collar</h3>
         <p className="price">$50 NZD</p>
         <img src="Collar.png" alt="Woofz Collar" />
-        <button>Add to Cart</button>
+       
       </div>
       <div className="card">
         <h3>Comfort Leash set</h3>
         <p className="price">$30 NZD</p>
-        <img src="Comfort-Leash.jpeg" alt="Comfort Leash set" />
-        <button>Add to Cart</button>
+        <img src="Comfort-Leash.png" alt="Comfort Leash set" />
+        
       </div>
       <div className="card">
         <h3>Luxe Leash</h3>
         <p className="price">$40 NZD</p>
-        <img src="Luxe-Leash.jpg" alt="Luxe Leash" />
-        <button>Add to Cart</button>
+        <img src="Luxe-Leash.png" alt="Luxe Leash" />
+        
       </div>
       <div className="card">
         <h3>Ultimate Bundle</h3>
         <p className="price">$99 NZD</p>
-        <img src="TrendyWoofz.jpg" alt="Brand logo" />
-        <button>Add to Cart</button>
+        <img src="TrendyWoofz.png" alt="Brand logo" />
+      
       </div>
       <Footer />
     </>
