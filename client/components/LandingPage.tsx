3e206ed4786819fe2ccf16997a4ec451ed39606b
Maddy Rio <madison-rio@hotmail.com>
2024-06-09 12:48:41 +1200

    Add projects footer css

--- client/components/LandingPage.tsx
+++ client/components/LandingPage.tsx
@@ -7,7 +7,7 @@ function Main() {
         <img onClick={() => window.location.href = "/about"} src="Profile.jpeg"></img>
       <div className="landingPage-text">
       <h2>Maddy Rio</h2>
-      <h3>Front-end Developer</h3>
+      <h3>Software Developer</h3>
       <p>On a mission to develop my skills and
     make a <strong>difference.</strong></p>
       </div>
